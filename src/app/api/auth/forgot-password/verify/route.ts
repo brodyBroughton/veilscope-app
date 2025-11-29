2c225c0b27be7f6f593fca5badfb6cd5965630af
Brody Broughton <broughton.brody07@gmail.com>
2025-11-29 14:58:38 -0700

    Trying to fix prisma client issues with passwordReset column

--- src/app/api/auth/forgot-password/verify/route.ts
+++ src/app/api/auth/forgot-password/verify/route.ts
@@ -6,7 +6,7 @@ export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+const globalForPrisma = global as unknown as { prisma?: any };
 
 const prisma =
   globalForPrisma.prisma ||
